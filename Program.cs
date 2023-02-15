b83905dbf5e3bb94bba6b3d097e8f0d0ef2ab878
E.Mdivani <E.Mdivani@EMDIVANI>
2023-02-15 17:20:51 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Eka
+{
+    static void Main(string[] args)
+    {
+
+        //ეს მონაწილეობს მისალმებისთვის
+        Console.WriteLine("Gamarjoba, me var Eka Mdivani;");
+        Console.WriteLine("var 37 wlis;");
+
+        //ეს დავალებას ეხება
+
+
+        Console.WriteLine("me var damckebi programisti;");
+        Console.WriteLine("minda gavxde magari programisti.");
+
+    }
+
+
+
+
+    /// <summary>
+    /// ეს ფორმულა არ იღებს მონაწილეობას
+    /// </summary>
+static void regularMethod()
+{
+
+    Console.WriteLine("me var proekt menejeri");
+    Console.WriteLine("bankshi mushaobis 15 cliani gamocdileba");
+
+}
+
+}
